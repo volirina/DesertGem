c273db00e749142b9af3ccbe78d9bf134d95ec68
ET-wont-phone-home <ermorinville@hotmail.fr>
2022-10-03 21:40:58 -0400

    inventory hotfix
    fixed the bug when picking item and inventory wasnt updated when opened

--- Assets/Scripts/UI/ItemPickUp.cs
+++ Assets/Scripts/UI/ItemPickUp.cs
@@ -6,11 +6,18 @@ public class ItemPickUp : MonoBehaviour
 {
     public Item Item;
 
-    // Add item to InventoryManager and delete item from scene
+
     void Pickup()
     {
+        //Add item to inventory
         InventoryManager.Instance.Add(Item);
+
+        //Destroy pickable item
         Destroy(gameObject);
+
+        //update inventory if iopen
+        InventoryManager.Instance.ListItems();
+
     }
 
     // Condition for pickup. Currently on mouse click
@@ -19,6 +26,14 @@ public class ItemPickUp : MonoBehaviour
         Pickup();
     }
 
-    
+    /*void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Pickup();
+        }
+
+    }*/
+
 
 }
